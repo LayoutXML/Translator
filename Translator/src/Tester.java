581d963062bd2218f6f7d6879991cdf7b94abb46
LayoutXML <hello@rokasjankunas.com>
2019-03-07 22:21:08 +0000

    More features

---
+++ Translator/src/Tester.java
@@ -0,0 +1,141 @@
+import java.util.Calendar;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+@SuppressWarnings("Duplicates")
+public class Tester {
+
+    private Translator translator;
+
+    public static void main(String[] args) {
+        Tester tester = new Tester();
+        tester.initialise();
+        tester.process();
+    }
+
+    public void initialise() {
+        translator = new Translator();
+        translator.initialise();
+    }
+
+    /**
+     * Method that acts on the user input
+     */
+    public void process() {
+        int userChoice;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            displayMenu();
+            try {
+                userChoice = scanner.nextInt();
+            }
+            catch (InputMismatchException e) {
+                userChoice=-1;
+                scanner = new Scanner(System.in); //recreating scanner because otherwise nextInt would not work
+                System.out.println("Invalid input. Please try again");
+            }
+            switch (userChoice) {
+                case 1:
+                    scanner = new Scanner(System.in); //recreating scanner for nextLine
+                    String input;
+                    try {
+                        System.out.println("Enter text in English: ");
+                        input = scanner.nextLine();
+                        String[] words = input.split(" ");
+                        long startTime = Calendar.getInstance().getTimeInMillis();
+                        for (String word : words) {
+                            System.out.print(translator.translate(word)+" ");
+                        }
+                        long endTime = Calendar.getInstance().getTimeInMillis();
+                        double wordsPerSecond = words.length*(1d/(endTime-startTime)*1000); //TODO: fix/improve speed calculation
+                        System.out.println("\nSpeed: "+wordsPerSecond+" words per second.");
+                    }
+                    catch (InputMismatchException e) {
+                        userChoice=-1;
+                        scanner = new Scanner(System.in); //recreating scanner because otherwise nextInt would not work
+                        System.out.println("Invalid input. Please try again");
+                    }
+                    break;
+                case 2:
+                    scanner = new Scanner(System.in); //recreating scanner for nextLine
+                    String input1, input2;
+                    try {
+                        System.out.println("Enter text in English: ");
+                        input1 = scanner.nextLine();
+                        System.out.println("Enter text in other language: ");
+                        input2 = scanner.nextLine();
+                        translator.addToDictionary(input1,input2);
+                    }
+                    catch (InputMismatchException e) {
+                        userChoice=-1;
+                        scanner = new Scanner(System.in); //recreating scanner because otherwise nextInt would not work
+                        System.out.println("Invalid input. Please try again");
+                    }
+                    break;
+                case 3:
+                    scanner = new Scanner(System.in); //recreating scanner for nextLine
+                    String input3;
+                    try {
+                        System.out.println("Enter text in English: ");
+                        input3 = scanner.nextLine();
+                        translator.removeFromDictionary(input3);
+                    }
+                    catch (InputMismatchException e) {
+                        userChoice=-1;
+                        scanner = new Scanner(System.in); //recreating scanner because otherwise nextInt would not work
+                        System.out.println("Invalid input. Please try again");
+                    }
+                    break;
+                case 4:
+                    scanner = new Scanner(System.in); //recreating scanner for nextLine
+                    String input4;
+                    try {
+                        System.out.println("Enter text in other language: ");
+                        input4 = scanner.nextLine();
+                        translator.removeFromDictionaryByValue(input4);
+                    }
+                    catch (InputMismatchException e) {
+                        userChoice=-1;
+                        scanner = new Scanner(System.in); //recreating scanner because otherwise nextInt would not work
+                        System.out.println("Invalid input. Please try again");
+                    }
+                    break;
+                case 5:
+                    scanner = new Scanner(System.in); //recreating scanner for nextLine
+                    String input5;
+                    try {
+                        System.out.println("Enter file name without txt extension: ");
+                        input5 = scanner.nextLine();
+                        translator.translateFile(input5);
+                    }
+                    catch (InputMismatchException e) {
+                        userChoice=-1;
+                        scanner = new Scanner(System.in); //recreating scanner because otherwise nextInt would not work
+                        System.out.println("Invalid input. Please try again");
+                    }
+                    break;
+                case 0:
+                    System.out.println("\nGoodbye...");
+                    break;
+                default:
+                    System.out.println("\nInvalid option. Please try again");
+                    break;
+            }
+        } while(userChoice!=0);
+    }
+
+    /**
+     * displayMenu method that displays all available options with their corresponding keyboard input
+     */
+    private void displayMenu() {
+        System.out.println("\nPlease select one of the options below:");
+        System.out.println("1. Translate user input");
+        System.out.println("2. Add a word or phrase to the dictionary");
+        System.out.println("3. Remove a word or phrase (English input)");
+        System.out.println("4. Remove a word or phrase (other language input)");
+        System.out.println("5. Translate a file");
+        //TODO: add automated tests
+        System.out.println("0. Exit");
+    }
+
+}
--- Translator/src/Translator.java
+++ Translator/src/Translator.java
@@ -2,30 +2,14 @@ import java.io.*;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
+@SuppressWarnings("Duplicates")
 public class Translator {
 
     private HashMap<String, String> dictionary;
     private boolean fileRead;
     private boolean isReading;
-
-    public static void main(String[] args) {
-        //main
-        Translator translator = new Translator();
-        translator.initialise();
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] words = input.split(" ");
-        System.out.println("");
-        long startTime = Calendar.getInstance().getTimeInMillis();
-        for (String word : words) {
-            System.out.print(translator.translate(word)+" ");
-        }
-        long endTime = Calendar.getInstance().getTimeInMillis();
-        double wordsPerSecond = words.length*(1d/(endTime-startTime)*1000);
-        System.out.println("Speed: "+wordsPerSecond+" words per second.");
-    }
+    private boolean isAddNewWordsToDictOptionEnabled; //TODO: react to this option
 
     public void initialise() {
         dictionary = new HashMap<>();
@@ -43,15 +27,78 @@ public class Translator {
         }
     }
 
+    public void translateFile(String fileName) {
+        FileReader fileReader;
+        BufferedReader bufferedReader;
+        StringBuilder text = new StringBuilder(); //StringBuilder for better performance (appending text in loop)
+        try {
+            fileReader = new FileReader(fileName+".txt");
+            bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+            while((line = bufferedReader.readLine()) != null) {
+                text.append(line);
+            }
+
+            bufferedReader.close();
+
+            String[] words = text.toString().split(" "); //TODO: better splitting algorithm (phrases + punctuation)
+            long startTime = Calendar.getInstance().getTimeInMillis();
+            for (String word : words) {
+                System.out.print(translate(word)+" ");
+            }
+            long endTime = Calendar.getInstance().getTimeInMillis();
+            double wordsPerSecond = words.length*(1d/(endTime-startTime)*1000);
+            System.out.println("\nSpeed: "+wordsPerSecond+" words per second.");
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+
     public void addToDictionary(String original, String translation) {
-        dictionary.put(original.toLowerCase(), translation.toLowerCase());
+        String oldValue = dictionary.put(original.toLowerCase(), translation.toLowerCase());
+        if (oldValue!=null) {
+            System.out.println("\""+original+"\"-\""+translation+"\" overrode previous pair \""+original+"\"-\""+oldValue+"\".");
+        } else {
+            System.out.println("\""+original+"\"-\""+translation+"\" added.");
+        }
+        writeFile();
     }
 
     public void removeFromDictionary(String original) {
-        dictionary.remove(original.toLowerCase());
+        String oldValue = dictionary.remove(original.toLowerCase());
+        if (oldValue!=null) {
+            System.out.println("\""+original+"\"-\""+oldValue+"\" removed.");
+        } else {
+            System.out.println("\""+original+"\" was not in the dictionary, so nothing was removed.");
+        }
+        writeFile();
     }
 
-    public void readFile() {
+    public void removeFromDictionaryByValue(String translation) {
+        //TODO: iterate through hashmap (dictionary) and remove element once value matches translation
+    }
+
+    /**
+     * Method to retrieve isAddNewWordsToDictOptionEnabled
+     *
+     * @return value of isAddNewWordsToDictOptionEnabled
+     */
+    public boolean isAddNewWordsToDictOptionEnabled() {
+        return isAddNewWordsToDictOptionEnabled;
+    }
+
+    /**
+     * Method to set isAddNewWordsToDictOptionEnabled
+     *
+     * @param addNewWordsToDictOptionEnabled value
+     */
+    public void setAddNewWordsToDictOptionEnabled(boolean addNewWordsToDictOptionEnabled) {
+        isAddNewWordsToDictOptionEnabled = addNewWordsToDictOptionEnabled;
+    }
+
+    private void readFile() {
         isReading = true;
         fileRead = false;
         Thread thread = new Thread() {
@@ -70,10 +117,11 @@ public class Translator {
                     while((line = bufferedReader.readLine()) != null) {
                         String[] words = line.split("\t",2);
                         dictionary.put(words[1].toLowerCase(),words[0].toLowerCase());
+                        //TODO: ensure 1:1 relationship, possibly implement this in addToDictionary and use it here
                     }
 
                     bufferedReader.close();
-                } catch (IOException | NumberFormatException e) {
+                } catch (IOException e) {
                     System.out.println("Error reading file");
                 } finally {
                     fileRead = true;
@@ -85,7 +133,7 @@ public class Translator {
         thread.run();
     }
 
-    public void writeFile() {
+    private void writeFile() {
         if (!isReading) {
             Thread thread = new Thread() {
                 @Override
@@ -110,5 +158,9 @@ public class Translator {
             };
             thread.run();
         }
+        //TODO: if isReading then write file asap when it turns false
+        //one way to implement this would be to create a variable isWaitingToWrite
+        //and a wrapper method for setIsReading which then checks if isWaitingToWrite when called
+        //and calls this method
     }
 }
